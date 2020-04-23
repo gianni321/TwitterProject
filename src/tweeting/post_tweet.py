7e952799c089aefc0002a06f258c17805411afc3
zwikel <zacharywikel@gmail.com>
2020-04-23 15:40:59 -0600

    oiioio

--- src/tweeting/post_tweet.py
+++ src/tweeting/post_tweet.py
@@ -17,7 +17,6 @@ The current date will be appended to the begining of the tweet.
 @WARNING This code will not be able to post the same status twice. @Fix by adding in date.
 '''
 
-# you fucker
 
 def parse_weather(grid):
     # string that will contain current weather information
