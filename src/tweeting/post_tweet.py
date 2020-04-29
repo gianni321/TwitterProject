d3315bdb6bb8c45bd930121f18b163e2f5ee8661
Zach <59543448+zwikel@users.noreply.github.com>
2020-04-29 14:57:02 -0600

    =)

--- src/tweeting/post_tweet.py
+++ src/tweeting/post_tweet.py
@@ -40,7 +40,9 @@ def parse_weather(grid):
 
         # Get icon and detailed forecast
         image_url = text["properties"]["periods"][0]["icon"][:-6]+"large"
-            str(text["properties"]["periods"][0]["temperature"]) + " F\n"
+        weather_report = "Weather Conditions At: " + str(grid) + " \n"
+        weather_report += "Current Temperature: " + str(text["properties"]["periods"][0]["temperature"]) + " F\n"
+        weather_report += "Forecast: " + text["properties"]["periods"][0]["detailedForecast"]
 
         # Write weather image
         with urlopen(image_url) as response:
